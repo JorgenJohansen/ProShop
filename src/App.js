42b16099505c9692166ce06913eb13974d420df2
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-08-02 13:41:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Container } from "react-bootstrap";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomePage />
+
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
